8c006f8d9a74668d8c910006c3ca029004db0318
AdrianPC <adrjeness@gmail.com>
2023-07-23 17:03:08 +0200

    The menu component is ready. Preparation of the card component. The expand feature is now available.

--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -3,6 +3,7 @@
 import React, {useState} from 'react';
 import Image from 'next/image';
 import Link from "next/link";
+import CartIcon from "@/app/components/CartIcon";
 
 const links = [
     { id: 1, title: 'Homepage', url: '/' },
@@ -17,19 +18,23 @@ const Menu = () => {
     return (
         <div>
             {
-                !open ?  <Image src="/open.png" alt="categoryicon" width={20} height={20} onClick={() => setOpen(true)}/>  :
+                !open ?  <Image src="/open.png" alt="categoryicon" width={20} height={20} onClick={() => setOpen(true)}/>
+                    :
                      <Image src="/close.png" alt="categoryicon" width={20} height={20} onClick={() => setOpen(false)}/>
             }
-            <div className="bg-orange-500 text-white absolute left-0 top-24 w-full h-[calc(100vh-6rem)] flex flex-col gap-8 items-center justify-center text-3xl z-10">
+            { open && <div className="bg-orange-500 text-white absolute left-0 top-24 w-full h-[calc(100vh-6rem)] flex flex-col gap-8 items-center justify-center text-3xl z-10">
                 {links.map(item => (
-                    <Link href={item.url} key={item.id}>{item.title}</Link>
+                    <Link href={item.url} key={item.id} onClick={() => setOpen(false)}>{item.title}</Link>
                 ))}
-                {!user ? <Link href="/login">Login</Link> :
-                <Link href="/orders">Orders</Link>}
-                <Link href="/cart"></Link>
-            </div>
+                {
+                    !user ? <Link href="/login" onClick={() => setOpen(false)}>Login</Link> :
+                            <Link href="/orders" onClick={() => setOpen(false)}>Orders</Link>
+                }
+                <Link href="/cart" onClick={() => setOpen(false)}>
+                    <CartIcon/>
+                </Link>
+            </div> }
         </div>
-
     )
 }
 export default Menu;
